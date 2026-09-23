6a7bbe145a4dcaa9e3b897b9ff8e94def03ba91c
aniketlahase54 <aniketlahase122@gmail.com>
2026-09-23 16:24:01 +0530

    add conditional statement

--- JavaScript Tutorial Part 2/Conditional-Statements.js
+++ JavaScript Tutorial Part 2/Conditional-Statements.js
@@ -1,1 +1,46 @@
-console.log("hello word")
+let age = 12;
+
+if (age >= 18){
+    console.log("we can vote");
+}
+else{
+    console.log("we cannot vote");
+}
+
+
+let num = 12;
+
+if (num % 2 === 0){
+    console.log("Num is even");
+}
+else{
+    console.log("Num is odd");
+}
+
+else if statements
+
+let mode = "dark";
+let color;
+
+if (mode === "dark"){
+    color ="black";
+}
+else if (mode === "blue"){
+    color = "blue";
+}
+else if (mode === "pink"){
+    color = "pink";
+}
+else{
+    color = "white"
+}
+
+console.log(color)
+
+
+Ternary operators
+let age = 24;
+
+let result = age > 18 ? "adult":"not adult";
+
+console.log(result)
